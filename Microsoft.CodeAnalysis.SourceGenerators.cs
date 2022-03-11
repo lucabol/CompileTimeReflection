33641d0302dd809b9205aa6530365752b63a450d
Luca Bolognese <lucabol@microsoft.com>
2022-03-11 11:09:15 +0100

    Add project files.

---
+++ Microsoft.CodeAnalysis.SourceGenerators.cs
@@ -0,0 +1,46 @@
+﻿namespace Microsoft.CodeAnalysis.CSharp.SourceGenerators.CompileTime;
+
+public class CompileTimeAttribute: Attribute { }
+
+public static class CompileTimeReflection
+{
+	public static Type[] GetTypes() =>
+        System.Reflection.Assembly.GetCallingAssembly().GetTypes();
+	public static Type GetType(string typeName) =>
+        System.Reflection.Assembly.GetCallingAssembly().GetType(typeName)!;
+}
+
+public class CompileTimeMethod
+{
+    public void AddSwitch(string switcher, IEnumerable<(string, string)> cases) { }
+}
+public class CompileTimeClass
+{
+    public CompileTimeMethod AddStaticMethod(string name, string args) => new CompileTimeMethod();
+}
+
+public static class CompileTimeEmit
+{
+    public static void EmitEnum(string name, IEnumerable<string> values) { }
+    public static CompileTimeClass EmitClass(string name) => new CompileTimeClass();
+}
+
+public enum Token { Double, Plus1, NotFound, Bye }
+
+public static class VmExt
+{
+    public static Token? FindToken(string name) {
+        var found = Enum.TryParse<Token>(name, out var result);
+        return found ? result : null;
+    }
+    public static void  ExecToken(Vm vm, Token token)
+    {
+        switch(token)
+        {
+            case Token.Plus1    : vm.Op_Plus1(); break; 
+            case Token.Double   : vm.Op_Double(); break;
+            case Token.Bye      : vm.Op_Bye();break;
+            case Token.NotFound : vm.Op_NotFound(); break;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// Importing the CompileTime namespace to gain access to compile time capabilities.
+using Microsoft.CodeAnalysis.CSharp.SourceGenerators.CompileTime;
+
+// Main interpreter loop, understands 'Double', 'Plus1', 'Bye'
+Vm vm = new();
+while(true)
+{
+    var line = Console.ReadLine();
+    if(line is null) break;
+    
+    var words = line.Split(' ');
+    foreach (var word in words)
+    {
+        // 'Token', 'FindToken' and 'ExecToken' are generated at compile time
+        // reflecting on the methods in the 'Vm' class in the 'CompileTimeExecutor'.
+        Token token = VmExt.FindToken(word) ?? Token.NotFound;
+        VmExt.ExecToken(vm, token);
+    }
+    Console.WriteLine($"State: {vm.state}");
+}
+
+// Adding a method to the 'Vm' class causes the interpreter to understand it.
+// You don't have to add a new value to the Token enum or code to handle user input for it.
+public class Vm
+{
+   public  int state;
+    
+    public void Op_Double()    => state *= 2;
+    public void Op_Plus1()     => state += 1;
+    public void Op_NotFound()  => Console.WriteLine("Word not found.");
+    public void Op_Bye()       => Environment.Exit(0);
+}
+
+// A class marked with the `CompileTime` attribute causes the compiler to execute
+// its `CompileTimeExecute' method at compile time.
+[CompileTime]
+public class CompileTimeExecutor
+{
+    public void CompileTimeExecute()
+    {
+        // Compile time reflection API. Get all the 'Op_*' methods from Vm.
+        var compileVm = CompileTimeReflection.GetType("Vm");
+        var opMethods = compileVm.GetMethods().Where(m => m.Name.StartsWith("Op_")); 
+        var words = opMethods.Select(m => m.Name[3..].ToLowerInvariant());
+
+        // Compile time reflection emit API. Generate the Enum.
+        CompileTimeEmit.EmitEnum("Token", words);
+
+        // Generate the class 'VmExt' with the two methods that we need.
+        var vmExt = CompileTimeEmit.EmitClass("VmExt");
+
+        var findToken = vmExt.AddStaticMethod("FindToken", "Token token");
+        findToken.AddSwitch("token", words.Select(w => (w, $"return Token.{w}")));
+
+        var exectToken = vmExt.AddStaticMethod("ExectToken", "Vm vm, Token token");
+        exectToken.AddSwitch("token", words.Select(w => ($"Token.{w}", $"vm.{w}()")));
+    }
+}
+
